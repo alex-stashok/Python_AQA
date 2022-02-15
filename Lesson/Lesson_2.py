a72580562c29002334078fa40cc31bebaf043ae8
Alex Stashok <i.casillas@mail.ru>
2022-02-15 12:57:55 +0200

    Add Line in Lesson_2

--- Lesson/Lesson_2.py
+++ Lesson/Lesson_2.py
@@ -42,3 +42,131 @@ dict_2 = {
 
 my_list = [dict_1, dict_2]
 print (my_list[1]["model"])
+
+x = 41
+if x > 10:
+    print ("Above ten,")
+    if x > 20:
+        print ("and also above 20!")
+    else:
+        print ("but not above 20.")
+
+# while  Break
+
+i = 1
+while True:
+    print (i)
+    if i == 3:
+        break
+    i += 1
+
+# while Continue
+
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+     continue
+    print (i)
+
+# while Continue
+
+i = 0
+while i < 6:
+    i += 1
+    if i in [3,5]:
+     continue
+    print (i)
+
+# while Pass
+
+i = 0
+while i < 6:
+    i += 1
+    if i in [3,5]:
+     pass
+    print (i)
+
+# while Continue
+
+i = 0
+my_list = [3, 5]
+while i < 6:
+    i += 1
+    if i in my_list:
+     continue
+    print (i)
+
+# while Continue
+
+i = 0
+my_limit = 6
+my_list = [3, 5]
+while i < my_limit:
+    i += 1
+    if i in my_list:
+     continue
+    print (i)
+
+# For
+
+fruits = ["apple", "banana", "cherry"]
+
+for x in fruits:
+    print (x)
+
+# For
+
+fruits = ["apple", "banana", "cherry"]
+new_list =[]
+for x in fruits [1]:
+    new_list.append (x)
+    print ("".join(new_list))
+
+# For
+
+fruits = ["apple", "banana", "cherry"]
+new_list =[]
+for x in range (0, 3, 1):
+    print (fruits [x])
+
+# Def
+
+def my_function (a, b):
+    c = a + b
+    return c
+
+first_val = 5
+second_val = 7
+
+result = my_function (first_val, second_val)
+
+print (result)
+
+# Def
+
+def my_function (a, b=2):
+    c = a + b
+    return c
+
+first_val = 5
+second_val = 7
+
+result = my_function (first_val, second_val)
+
+print (result)
+
+# Def
+
+def my_function (a, b=2):
+    c = a + b
+    return c
+
+first_val = 5
+second_val = 7
+
+result = my_function (first_val, second_val)
+
+print (result)
+
+
